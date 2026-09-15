93c97e6336b184694535fbba82bcca714836c595
Zsolt Sipeki <sipekizs90@gmail.com>
2026-09-15 11:25:14 +0200

    Added .env

---
+++ video_stats.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+
+API_KEY = os.getenv('API_KEY')
+CHANNEL_HANDLE = 'magyarosi'
+
+def get_playlist_id():
+
+    try:
+
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+        channel_items = data['items'][0]
+        channel_playListId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+
+        # print(data['items'][0]['contentDetails']['relatedPlaylists']['uploads'])
+
+        return channel_playListId
+
+    except requests.exceptions.RequestException as e:
+            raise e
+
+if __name__ == '__main__':
+    print(get_playlist_id())
